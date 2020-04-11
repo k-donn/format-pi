4964b1f08bd255d146789811932d819a7f0df961
k-donn <mitchell@kevinandjudy.com>
2020-04-11 09:51:38 -0400

    Created wheel and uploaded

--- build/lib/matplot_fmt_pi/__init__.py
+++ build/lib/matplot_fmt_pi/__init__.py
@@ -1,4 +1,16 @@
+"""
+matplot_fmt_pi
 
+Format numbers as multiples of pi for matplotlib plots.
+
+The class MultiplePi contains the math and formatting to accomplish this.
+MultiplePi has methods to return a locator and formatter for use on a plot.
+
+
+classes include:
+    MultiplePi
+        contains everything needed
+"""
 from .multiple import MultiplePi
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
