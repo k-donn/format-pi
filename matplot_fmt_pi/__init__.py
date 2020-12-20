7d2fa42873d7785ee9235c5bb59c9ad04e18d2f3
k-donn <mitchell@kevinandjudy.com>
2020-12-20 18:00:43 -0500

    Bump version: 2.0.3 → 2.0.4

--- matplot_fmt_pi/__init__.py
+++ matplot_fmt_pi/__init__.py
@@ -22,6 +22,6 @@ In addition, it forces use of its own cos, sin, etc.
 while this is far more extensible.
 """
 
-__version__ = "2.0.3"
+__version__ = "2.0.4"
 __name__ = "matplot_fmt_pi"
 __pie__ = "Tastes good"
